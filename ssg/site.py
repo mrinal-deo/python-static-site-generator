7f3a0d8448cc631b53e8a0e9f7dd6a6b2280c263
Mrinal Deo <mrinal-deo@users.noreply.github.com>
2021-02-24 14:11:31 +0900

    Commit to test in pluralsight

--- ssg/site.py
+++ ssg/site.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 
-
 class Site:
     def __init__(self, source, dest):
         self.source = Path(source)
@@ -8,10 +7,9 @@ class Site:
 
     def create_dir(self, path):
         directory = self.dest / path.relative_to(self.source)
-        directory.mkdir(parents=True, exist_ok=True)
+        Path.mkdir(directory, True, True)
 
     def build(self):
-        self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
