f7a961a92a45920d0c89e09648bebc4c3af0864d
Mrinal Deo <mrinal-deo@users.noreply.github.com>
2021-02-25 13:52:23 +0900

    Module 3 Solution

--- ssg.py
+++ ssg.py
@@ -2,7 +2,7 @@ import typer
 from ssg.site import Site
 import ssg.parsers
 
-def main(source="content", dest ="dist"):
+def main(source="content", dest="dist"):
     config = {
         "source": source,
         "dest": dest,
